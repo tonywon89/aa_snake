0aaef7d62c536f61e59668fa44a6f356323e1ff7
Tony Won <tonywon89@gmail.com>
2016-04-13 15:42:34 -0700

    implement view render

--- js/bundle.js
+++ js/bundle.js
@@ -124,33 +124,58 @@
 	  this.board = new Board;
 
 	  this.bindKeys();
+	  this.render();
 	}
 
 	View.prototype.bindKeys = function(){
 	  var self = this;
 	  this.$el.on("keydown", function(event){
-	    var pressedKey = event.which;
-
-	    switch (pressedKey) {
-	    case 37:
-	      pressedKey = "W";
-	      break;
-	    case 38:
-	      pressedKey = "N";
-	      break;
-	    case 39:
-	      pressedKey = "E";
-	      break;
-	    case 40:
-	      pressedKey = "S";
-	      break;
-	    }
-	    self.board.snake.turn(pressedKey);
-	    // TODO remove this line after implementing elsewhere
-	    self.board.snake.move();
-	    console.log(self.board.snake);
+	    var pressedKey = event.keyCode;
+	    self.handleKeyEvent(pressedKey);
 	  });
 	};
+
+	View.prototype.handleKeyEvent = function (pressedKey) {
+	  switch (pressedKey) {
+	  case 37:
+	    pressedKey = "W";
+	    break;
+	  case 38:
+	    pressedKey = "N";
+	    break;
+	  case 39:
+	    pressedKey = "E";
+	    break;
+	  case 40:
+	    pressedKey = "S";
+	    break;
+	  }
+
+	  this.board.snake.turn(pressedKey);
+	  // TODO remove this line after implementing elsewhere
+	  this.board.snake.move();
+	  console.log(this.board.snake.segments[0]);
+	};
+
+	View.prototype.step = function () {
+	  this.board.snake.move();
+	  this.render();
+	};
+
+	View.prototype.render = function () {
+	  var $ul = $("<ul>").addClass("grid group");
+
+	  for(var i = 0; i < 10; i++){
+	    for(var j = 0; j < 10; j++){
+	      var $li = $("<li>").addClass("grid-box");
+	      $ul.append($li);
+	    }
+	  }
+
+	  $(".snake-game").append($ul);
+	};
+
+
 
 	module.exports = View;
 
